02eb6e4829e3f8affbbde1628b3bd2da8cfd037e
Amar Pawar <pawaramar.pawar@gmail.com>
2021-07-20 17:49:30 +0530

    [Amar Pawar] Add: Function to create left join between two tables

--- JoinFunction/join_functions.py
+++ JoinFunction/join_functions.py
@@ -1,3 +1,12 @@
+'''
+/**********************************************************************************
+@Author: Amar Pawar
+@Date: 2021-07-20
+@Last Modified by: Amar Pawar
+@Last Modified time: 2021-07-20
+@Title : Program to perform various join operations of database tables
+/**********************************************************************************
+'''
 import sys
 sys.path.insert(0, '/home/ubuntu/Documents/PythonWorkspace/MySQL-Operations')
 from CrudOperations.logging_handler import logger
@@ -32,7 +41,7 @@ class JoinFunction():
     def right_join(self):
         """
         Description:
-            This function will create inner join between two tables and find intersecting values.
+            This function will create right join between two tables.
         """
         try:
             my_cursor = db_connection.cursor()
@@ -43,8 +52,21 @@ class JoinFunction():
         except Exception as e:
             logger.info(f"Errorr!!{e}")
 
+    def left_join(self):
+        """
+        Description:
+            This function will create left join between two tables.
+        """
+        try:
+            my_cursor = db_connection.cursor()
+            left_join_query ="select employee_details.id,employee_details.name,employee_details.salary,department.department_id,department.dept_name from department left join employee_details on department.department_id = employee_details.department_id"
+            my_cursor.execute(left_join_query)
+            result = my_cursor.fetchall()
+            logger.info(result)
+        except Exception as e:
+            logger.info(f"Errorr!!{e}")
+
 join_object = JoinFunction()
 join_object.inner_join()
 join_object.right_join()
-
-
+join_object.left_join()
