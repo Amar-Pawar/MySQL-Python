63ea5f6c15c2dfdf563779eb713cfa68b7dd830e
Amar Pawar <pawaramar.pawar@gmail.com>
2021-07-21 08:37:29 +0530

    [Amar Pawar] Add:Function to display created view information

--- MysqlViews/mysql_views.py
+++ MysqlViews/mysql_views.py
@@ -25,7 +25,7 @@ db_connection = mysql.connector.connect(
 
 class MysqlViews():
 
-    def create_view():
+    def create_view(self):
         """
         Description:
             This function will create mysql view from given columns from table.
@@ -34,8 +34,24 @@ class MysqlViews():
             my_cursor = db_connection.cursor()
             create_view_query ="create view emp_info as select name,salary from employee_details"
             my_cursor.execute(create_view_query)
+            logger.info("View created")
         except Exception as e:
-            print(f"Errorr!!{e}")
+            logger.info(f"Errorr!!{e}")
+
+    def display_view(self):
+        """
+        Description:
+            This function will create mysql view from given columns from table.
+        """
+        try:
+            my_cursor = db_connection.cursor()
+            show_view_query = ("select * from emp_info")
+            my_cursor.execute(show_view_query)
+            result = my_cursor.fetchall()
+            logger.info(result)
+        except Exception as e:
+            logger.info(f"Errorr!!{e}")
 
 view_object = MysqlViews()
 view_object.create_view()
+view_object.display_view()
